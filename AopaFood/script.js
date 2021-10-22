35dcff6ee72e3ceecc01d922125b3c020a2fa26a
RyanDrop <ryangithub7509@gmail.com>
2021-10-22 10:28:35 -0300

    📦 refactor: Type module and slideshow refactor

--- AopaFood/script.js
+++ AopaFood/script.js
@@ -1,82 +1,19 @@
+import {MAIN_DISHES, DESSERTS, DRINKS} from "./modules/shop-items.js"
+import { currentSlideIndex, nextSlide, showSlides } from "./modules/slideshow.js";
+
 const $header = document.querySelector("header");
-const $slides = document.querySelectorAll(".slide");
-const $dots = document.querySelectorAll(".dot");
-const $prev = document.querySelector(".prev");
-const $next = document.querySelector(".next");
 const $mainCourse = document.querySelector(".main-dishes");
 const $dessert = document.querySelector(".desserts");
 const $drink = document.querySelector(".drinks");
-const $cartItens = document.querySelector(".cart-items");
+const $cartItems = document.querySelector(".cart-items");
 const formatDollObj = { style: "currency", currency: "USD" };
 const $modal = document.querySelector(".modal");
 const $cart = document.querySelector(".cart");
 const $buttonClose = document.querySelector(".close-cart");
 
-const mainDishes = [
-  {
-    id: 1,
-    description: "Rice",
-    value: 2.0,
-    quantity: 0,
-    img: "./assets/food-images/rice.png",
-  },
-  {
-    id: 2,
-    description: "Bean",
-    value: 1.6,
-    quantity: 0,
-    img: "./assets/food-images/bean.png",
-  },
-  {
-    id: 3,
-    description: "Salad",
-    value: 0.4,
-    quantity: 0,
-    img: "./assets/food-images/salad.png",
-  },
-];
-
-const desserts = [
-  {
-    id: 101,
-    description: "Ice Cream",
-    value: 0.99,
-    quantity: 0,
-    img: "./assets/food-images/ice-cream.png",
-  },
-  {
-    id: 102,
-    description: "Blueberry",
-    value: 1.1,
-    quantity: 0,
-    img: "./assets/food-images/blueberry.png",
-  },
-];
-
-const drinks = [
-  {
-    id: 201,
-    description: "Juice",
-    value: 2.0,
-    quantity: 0,
-    img: "./assets/food-images/orange-juice.png",
-  },
-  {
-    id: 202,
-    description: "Coke",
-    value: 2.0,
-    quantity: 0,
-    img: "./assets/food-images/coca-cola.png",
-  },
-];
-
 window.addEventListener("scroll", () => {
   $header.classList.toggle("scroll-color", window.scrollY);
 });
-$dots[0].addEventListener("click", () => currentSlide(0));
-$dots[1].addEventListener("click", () => currentSlide(1));
-$next.addEventListener("click", nextSlide);
-$prev.addEventListener("click", previousSlide);
 $cart.addEventListener("click", activeModal);
 $buttonClose.addEventListener("click", activeModal);
 
@@ -84,59 +21,7 @@ function activeModal() {
   $modal.classList.toggle("active");
 }
 
-let currentSlideIndex = 0;
 showSlides(currentSlideIndex);
-
-function nextSlide() {
-  hideSlides();
-  removeActiveClass();
-  currentSlideIndex = (currentSlideIndex += 1) % $slides.length;
-
-  const $currentSlide = $slides[currentSlideIndex];
-  const $currentDot = $dots[currentSlideIndex];
-
-  $currentSlide.style.display = "block";
-  $currentDot.classList.add("active");
-}
-
-function previousSlide() {
-  hideSlides();
-  removeActiveClass();
-  const nextIndex = (currentSlideIndex -= 1);
-  currentSlideIndex = nextIndex < 0 ? $slides.length - 1 : nextIndex;
-
-  const $currentSlide = $slides[currentSlideIndex];
-  const $currentDot = $dots[currentSlideIndex];
-
-  $currentSlide.style.display = "block";
-  $currentDot.classList.add("active");
-}
-
-function currentSlide(n) {
-  showSlides((currentSlideIndex = n));
-}
-
-function showSlides(currentIndex) {
-  hideSlides();
-  removeActiveClass();
-
-  $slides[currentSlideIndex].style.display = "block";
-  $dots[currentSlideIndex].classList.add("active");
-}
-
-function removeActiveClass() {
-  for (i = 0; i < $dots.length; i++) {
-    const $dot = $dots[i];
-    $dot.classList.remove("active");
-  }
-}
-
-function hideSlides() {
-  for (i = 0; i < $slides.length; i++) {
-    const $slide = $slides[i];
-    $slide.style.display = "none";
-  }
-}
 setInterval(nextSlide, 4000);
 
 function createElementWithClass(selector, className) {
@@ -161,22 +46,22 @@ function modelHTML(object) {
   return html;
 }
 
-for (i = 0; i < mainDishes.length; i++) {
-  const cardInnerHtml = modelHTML(mainDishes[i]);
+for (let i = 0; i < MAIN_DISHES.length; i++) {
+  const cardInnerHtml = modelHTML(MAIN_DISHES[i]);
   const $card = createCardElement();
   $card.innerHTML = cardInnerHtml;
   $mainCourse.appendChild($card);
 }
 
-for (i = 0; i < desserts.length; i++) {
-  const cardInnerHtml = modelHTML(desserts[i]);
+for (let i = 0; i < DESSERTS.length; i++) {
+  const cardInnerHtml = modelHTML(DESSERTS[i]);
   const $card = createCardElement();
   $card.innerHTML = cardInnerHtml;
   $dessert.appendChild($card);
 }
 
-for (i = 0; i < drinks.length; i++) {
-  const cardInnerHtml = modelHTML(drinks[i]);
+for (let i = 0; i < DRINKS.length; i++) {
+  const cardInnerHtml = modelHTML(DRINKS[i]);
   const $card = createCardElement();
   $card.innerHTML = cardInnerHtml;
   $drink.appendChild($card);

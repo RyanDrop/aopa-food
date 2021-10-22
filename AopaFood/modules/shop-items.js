703b970d2c8382db79582812cab02c4a3d7bceb7
RyanDrop <ryangithub7509@gmail.com>
2021-10-22 10:00:48 -0300

    ✨ feat: Shop-items module

---
+++ AopaFood/modules/shop-items.js
@@ -0,0 +1,59 @@
+export { MAIN_DISHES, DESSERTS, DRINKS };
+
+const MAIN_DISHES = [
+  {
+    id: 1,
+    description: "Rice",
+    value: 2.0,
+    quantity: 0,
+    img: "./assets/food-images/rice.png",
+  },
+  {
+    id: 2,
+    description: "Bean",
+    value: 1.6,
+    quantity: 0,
+    img: "./assets/food-images/bean.png",
+  },
+  {
+    id: 3,
+    description: "Salad",
+    value: 0.4,
+    quantity: 0,
+    img: "./assets/food-images/salad.png",
+  },
+];
+
+const DESSERTS = [
+  {
+    id: 101,
+    description: "Ice Cream",
+    value: 0.99,
+    quantity: 0,
+    img: "./assets/food-images/ice-cream.png",
+  },
+  {
+    id: 102,
+    description: "Blueberry",
+    value: 1.1,
+    quantity: 0,
+    img: "./assets/food-images/blueberry.png",
+  },
+];
+
+const DRINKS = [
+  {
+    id: 201,
+    description: "Juice",
+    value: 2.0,
+    quantity: 0,
+    img: "./assets/food-images/orange-juice.png",
+  },
+  {
+    id: 202,
+    description: "Coke",
+    value: 2.0,
+    quantity: 0,
+    img: "./assets/food-images/coca-cola.png",
+  },
+];
